b719a7b30d07c0cd52d86325d46feada0ba5a226
Alfitar <87951368+Alfitar@users.noreply.github.com>
2021-07-26 08:25:59 +0700

    Add files via upload

---
+++ Component/Jumbotron.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function Jumbotron() {
+  return (
+    <div>
+    <div className="container">
+        
+      <div className="jumbotron">
+        <h1 className="display-4">chipmunk_present</h1>
+        <p className="lead">
+          Jasa Layanan Fotografi Dan Vidiografi Termurah Di Banyuwngi
+        </p>
+        <hr className="my-4" />
+        <p>
+        "Jangan Kapok Mengunakan Jasa Layanan Kami"
+        </p>
+        <a className="btn btn-primary btn-lg" href="#" role="button">
+          Cek Paket Foto dan Vidio
+        </a>
+      </div>
+    </div>
+    </div>
+  );
+}
